79558974e051f38aafdc0313efbe561f7d8f5e96
Shawn Lin <shawn@3a.co.nz>
2015-07-20 10:50:06 +1200

    add base url setting

--- mysite/_config.php
+++ mysite/_config.php
@@ -9,6 +9,8 @@ $database = 'powermoney';
 // Use _ss_environment.php file for configuration
 require_once("conf/ConfigureFromEnv.php");
 
+Director::setBaseURL('/');
+
 // Set the site locale
 i18n::set_locale('en_US');
 //i18n::set_locale('zh_CN');
