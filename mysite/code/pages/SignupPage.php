fdb8da0faeb6e128b772f9c038ffc68bfcfcc865
Shawn Lin <jallen0927@hotmail.com>
2015-07-28 23:57:33 +1200

    Signup form style

--- mysite/code/pages/SignupPage.php
+++ mysite/code/pages/SignupPage.php
@@ -33,51 +33,55 @@ class SignupPage_Controller extends Page_Controller {
         $fields = new FieldList();
 
         $detailFields = new CompositeField(
+            LiteralField::create('Detail', '<h4 class="title">Your Detail</h4>'),
             DropdownField::create('Title', _t('Signup.Title', 'Title'), array('Mr', 'Miss', 'Mrs', 'Ms', 'Dr')),
             TextField::create('FirstName', _t('Signup.FirstName', 'First Name')),
             TextField::create('LastName', _t('Signup.LastName', 'Last Name')),
             TextField::create('Phone', _t('Signup.Phone', 'Phone')),
             TextField::create('Email', _t('Signup.Email', 'Email')),
+            LiteralField::create('Instruction', '<h5 class="title">For credit check purposes, please provide us with your NZ driver licence number and card version number, if possible.</h5>'),
             TextField::create('License', _t('Signup.License', 'License No.')),
             TextField::create('Version', _t('Signup.Version', 'License Version')),
             DateField::create('DateOfBirth', _t('Signup.DateOfBirth', 'Date Of Birth'))->setConfig('showcalendar', true)
         );
 
         $propertyFields = new CompositeField(
+            LiteralField::create('Property', '<h4 class="title">Property Details</h4>'),
             TextField::create('Address', _t('Signup.Address', 'Property'))->setAttribute('placeholder', _t('Signup.PropertyTip', 'Your property address')),
             OptionsetField::create('Primary', _t('Signup.Primary', 's this your primary address?'), array(
-                '1' => _t('Signup.Yes', 'Yes'),
-                '0' => _t('Signup.No', 'No')
+                'true' => _t('Signup.Yes', 'Yes'),
+                'false' => _t('Signup.No', 'No')
             )),
             OptionsetField::create('Services', _t('Signup.Services', 'How many people live in your household?'), array(
                 '1' => '1',
                 '2' => '2',
                 '3' => '3',
                 '4' => '4',
-                '4 or more' => '4' . _t('Signup.More', 'or more')
+                '4 or more' => '4 ' . _t('Signup.More', 'or more')
             )),
             OptionsetField::create('Situation', _t('Signup.Situation', 'What is your situation?'), array(
                 'Moving' => _t('Signup.Moving', "I'm moving or need a new connection"),
                 'Switch' => _t('Signup.Switch', 'I just want to switch energy providers')
             )),
             OptionsetField::create('Prevent', _t('Signup.Prevent', 'Is there anything that could prevent a meter reader accessing your meter, such as any dogs on the property?'), array(
-                '1' => _t('Signup.Yes', 'Yes'),
-                '0' => _t('Signup.No', 'No')
+                'true' => _t('Signup.Yes', 'Yes'),
+                'false' => _t('Signup.No', 'No')
             )),
             OptionsetField::create('Medical', _t('Signup.Medical', 'Is anyone at this property reliant on mains electricity for critical medical support?'), array(
-                '1' => _t('Signup.Yes', 'Yes'),
-                '0' => _t('Signup.No', 'No')
+                'true' => _t('Signup.Yes', 'Yes'),
+                'false' => _t('Signup.No', 'No')
             )),
             OptionsetField::create('Threat', _t('Signup.Threat', 'Does disconnection of electricity present a clear threat to the health or wellbeing of anyone at this property for reasons of age, health or disability?'), array(
-                '1' => _t('Signup.Yes', 'Yes'),
-                '0' => _t('Signup.No', 'No')
+                'true' => _t('Signup.Yes', 'Yes'),
+                'false' => _t('Signup.No', 'No')
             ))
         );
 
         $billingFields = new CompositeField(
+            LiteralField::create('Billing', '<h4 class="title">Billing Details</h4>'),
             OptionsetField::create('BillingSame', _t('Signup.BillingSame', 'Is your billing address the same as your supply address?'), array(
-                '1' => _t('Signup.Yes', 'Yes'),
-                '0' => _t('Signup.No', 'No')
+                'true' => _t('Signup.Yes', 'Yes'),
+                'false' => _t('Signup.No', 'No')
             )),
             TextField::create('Billing', _t('Signup.Billing', 'Billing address'))
         );
@@ -90,7 +94,23 @@ class SignupPage_Controller extends Page_Controller {
             FormAction::create('Submit', 'Submit')
         );
 
-        $required = new RequiredFields();
+        $required = new RequiredFields(
+            'Title',
+            'FirstName',
+            'LastName',
+            'Phone',
+            'Email',
+            'DateOfBirth',
+            'Address',
+            'Primary',
+            'Services',
+            'People',
+            'Situation',
+            'Prevent',
+            'Medical',
+            'Threat',
+            'BillingSame'
+        );
 
         $form = new Form($this, __FUNCTION__, $fields, $actions, $required);
 
--- themes/simple/templates/Layout/SignupPage.ss
+++ themes/simple/templates/Layout/SignupPage.ss
@@ -1,6 +1,9 @@
 <div class="row content">
     <div class="block col-xs-12 col-sm-10 col-sm-offset-1">
-        <div class="col-sm-6 col-sm-offset-3 col-xs-12">
+        <div class="col-sm-12 title text-center">
+            <h3>JOIN US TODAY!!!</h3>
+        </div>
+        <div class="col-sm-10 col-sm-offset-1 col-xs-12">
             $SignupForm
         </div>
     </div>
